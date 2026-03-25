923c2e9a056b9ebf04c59cad347b8dac645db918
Nethamine <nathan.phang123@gmail.com>
2026-03-25 19:28:31 +0000

    nog meer stijl

--- streamlit_app.py
+++ streamlit_app.py
@@ -716,7 +716,7 @@ with st.sidebar:
         help="Groter venster = soepelere features, iets minder precies.",
     )
 
-    st.markdown('<div class="speed-box-label">Snelheid vs nauwkeurigheid</div>', unsafe_allow_html=True)
+    st.markdown('<div class="speed-box-label">Model snelheid</div>', unsafe_allow_html=True)
 
     max_activiteiten = st.slider(
         "Max. activiteiten",
